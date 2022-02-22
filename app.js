ab191e567f7a93d7ee6ea4e6ee90bd27944a5868
ATIQUR RAHMAN <75171760+atiqurrahman-it@users.noreply.github.com>
2022-02-22 23:36:14 +0600

    background& complate task

--- app.js
+++ app.js
@@ -1,4 +1,29 @@
+// background color section start 
 
+const fext_div1=document.querySelector('.div_flex1')
+const fext_div2=document.querySelector('.div_flex2')
+const fext_div3=document.querySelector('.div_flex3')
+
+let completeUl = document.querySelector('.complete-list ul');
+
+let container=document.querySelector('.container')
+
+fext_div1.addEventListener('click',function(){
+    container.style.backgroundColor=`rgb(204, 5, 187)`;
+
+});
+
+fext_div2.addEventListener('click',function(){
+    container.style.backgroundColor=`rgb(212, 129, 4)`;
+
+});
+fext_div3.addEventListener('click',function(){
+    container.style.backgroundColor=`rgb(6, 144, 224)`;
+
+
+});
+
+// background color section End  
 
 const new_task=document.querySelector('#new-task')
 const addTask=document.querySelector('#addTask')
@@ -32,25 +57,100 @@ form.addEventListener('submit',function(event){
     event.preventDefault(); // defualt behabiar off 
 
     myvalidation();
-
-    let list_item=add_list_element(new_task.value);
-    item.appendChild(list_item);
-    new_task.value=""
+     // this blow code  work after myvalidation check 
+    // let list_item=add_list_element(new_task.value);
+    // item.appendChild(list_item);
+    
+    // new_task.value=""
+    // bindInCompleteItems(list_item,Completetask)
+    
     
 });
 
 
+let Completetask= function() {
+    let listItem = this.parentNode;  // this meens  click input flied
+    //this.parentNode meens  input er parent that meens li flied 
+    let deleteBtn = document.createElement('button');
+    deleteBtn.innerText = 'Delete';
+    deleteBtn.className = 'delete';
+    listItem.appendChild(deleteBtn);
+
+    let checkBox = listItem.querySelector('input[type="checkbox"]');
+    checkBox.remove();
+
+    completeUl.appendChild(listItem);
+
+    bindCompleteItems(listItem, deleteTask); //delete er jono
+    
+}
+
+let deleteTask = function() {
+    let listItem = this.parentNode;
+    let ul = listItem.parentNode;
+    ul.removeChild(listItem);
+}
+
+
+let bindInCompleteItems = function(taskItem, checkboxClick) {
+    let checkBox = taskItem.querySelector('input[type="checkbox"]');
+    checkBox.onchange = checkboxClick; // when box is check then function is runing (complate function e input filed jabe (this) )
+}
+
+// defual element compalte er joono
+for(let i=0; i< item.children.length; i++ ) {
+    bindInCompleteItems(item.children[i], Completetask);
+}
+
+
+let bindCompleteItems = function(taskItem, deleteButtonClick) {
+    let deleteButton = taskItem.querySelector('.delete');
+    deleteButton.onclick = deleteButtonClick;
+}
+
+
+// defual element delete  er joono
+for(let i=0; i< completeUl.children.length; i++ ) {
+
+    bindCompleteItems(completeUl.children[i], deleteTask);
+}
+
+
+
+
+
+
+// validation er jono  start 
 function myvalidation(){
     let values=new_task.value.trim()
-
     if(values ==""){
-        setErrorFor(new_task,'** Please fill up the baground color name!') //setErrorFor fucntion define korte hobe
-        
-
+        new_task.classList.add('inbutbox_error')
+        setErrorFor(new_task,'** Please fill up the Taks Name!') //setErrorFor fucntion define korte hobe
     }
     else{
-        
+        let list_item=add_list_element(new_task.value);
+        item.appendChild(list_item);
+        new_task.value=""
+        bindInCompleteItems(list_item,Completetask)
+
+        new_task.classList.remove('inbutbox_error')
+        new_task.classList.add('inbutbox_success')
+        setSuccess(new_task)
     }
-
-
 }
+
+function setErrorFor(values,mess){
+    let error_message=document.querySelector('.error_message')
+    error_message.innerHTML=mess;
+    error_message.style.color="red"
+    
+}
+function setSuccess(values){
+    let error_message=document.querySelector('.error_message')
+    error_message.innerHTML="";
+    
+}
+
+// validation er jono  End  
+
+

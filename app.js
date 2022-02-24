5ec5e40ee2ae2e43ef956de6f9df45a24492b4bc
ATIQUR RAHMAN <75171760+atiqurrahman-it@users.noreply.github.com>
2022-02-24 23:49:31 +0600

    second way

--- app.js
+++ app.js
@@ -53,6 +53,10 @@ function add_list_element(value_task){
 
 
 
+
+
+
+
 form.addEventListener('submit',function(event){
     event.preventDefault(); // defualt behabiar off 
 
@@ -68,55 +72,103 @@ form.addEventListener('submit',function(event){
 });
 
 
-let Completetask= function() {
-    let listItem = this.parentNode;  // this meens  click input flied
-    //this.parentNode meens  input er parent that meens li flied 
+// second way Start 
+let checkbox=document.querySelector('#items');
+checkbox.addEventListener('click',function(event){
+//    const single_click_item=event.target.parentElement
+
+  if ($(event.target).is('[type="checkbox"]')){
+    const single_click_item=event.target.parentElement
+
+    let cc=single_click_item.querySelector('input[type="checkbox"]');
+    cc.remove()
+ 
     let deleteBtn = document.createElement('button');
     deleteBtn.innerText = 'Delete';
     deleteBtn.className = 'delete';
-    listItem.appendChild(deleteBtn);
+    single_click_item.appendChild(deleteBtn);
+ 
+    let complete_item= single_click_item.cloneNode(true) //depth clone 
+    single_click_item.remove()
+ 
+    add_complate_item(complete_item)
 
-    let checkBox = listItem.querySelector('input[type="checkbox"]');
-    checkBox.remove();
+  }
+   
 
-    completeUl.appendChild(listItem);
+});
 
-    bindCompleteItems(listItem, deleteTask); //delete er jono
+// let completeUl = document.querySelector('.complete-list ul');
+ function add_complate_item(item){
+    completeUl.appendChild(item)
+
+}
+
+// delete for complete items 
+completeUl.addEventListener('click',function(e){
+    if(e.target.classList.contains('delete')){
+        let li=e.target.parentElement
+        li.remove()
+    }
+})
+
+
+// second way End 
+
+// frist way  Start 
+
+
+
+// let Completetask= function() {
+//     let listItem = this.parentNode;  // this meens  click input flied
+//     //this.parentNode meens  input er parent that meens li flied 
+//     let deleteBtn = document.createElement('button');
+//     deleteBtn.innerText = 'Delete';
+//     deleteBtn.className = 'delete';
+//     listItem.appendChild(deleteBtn);
+
+//     let checkBox = listItem.querySelector('input[type="checkbox"]');
+//     checkBox.remove();
+
+//     completeUl.appendChild(listItem);
+
+//     bindCompleteItems(listItem, deleteTask); //delete er jono
     
-}
+// }
 
-let deleteTask = function() {
-    let listItem = this.parentNode;
-    let ul = listItem.parentNode;
-    ul.removeChild(listItem);
-}
-
-
-let bindInCompleteItems = function(taskItem, checkboxClick) {
-    let checkBox = taskItem.querySelector('input[type="checkbox"]');
-    checkBox.onchange = checkboxClick; // when box is check then function is runing (complate function e input filed jabe (this) )
-}
-
-// defual element compalte er joono
-for(let i=0; i< item.children.length; i++ ) {
-    bindInCompleteItems(item.children[i], Completetask);
-}
+// let deleteTask = function() {
+//     let listItem = this.parentNode;
+//     let ul = listItem.parentNode;
+//     ul.removeChild(listItem);
+// }
 
 
-let bindCompleteItems = function(taskItem, deleteButtonClick) {
-    let deleteButton = taskItem.querySelector('.delete');
-    deleteButton.onclick = deleteButtonClick;
-}
+// let bindInCompleteItems = function(taskItem, checkboxClick) {
+//     let checkBox = taskItem.querySelector('input[type="checkbox"]');
+//     checkBox.onchange = checkboxClick; // when box is check then function is runing (complate function e input filed jabe (this) )
+// }
+
+// // defual element compalte er joono
+// for(let i=0; i< item.children.length; i++ ) {
+//     bindInCompleteItems(item.children[i], Completetask);
+// }
 
 
-// defual element delete  er joono
-for(let i=0; i< completeUl.children.length; i++ ) {
-
-    bindCompleteItems(completeUl.children[i], deleteTask);
-}
-
+// let bindCompleteItems = function(taskItem, deleteButtonClick) {
+//     let deleteButton = taskItem.querySelector('.delete');
+//     deleteButton.onclick = deleteButtonClick;
+// }
 
 
+// // defual element delete  er joono
+// for(let i=0; i< completeUl.children.length; i++ ) {
+
+//     bindCompleteItems(completeUl.children[i], deleteTask);
+// }
+
+
+
+// Frist way End  
 
 
 
@@ -131,7 +183,7 @@ function myvalidation(){
         let list_item=add_list_element(new_task.value);
         item.appendChild(list_item);
         new_task.value=""
-        bindInCompleteItems(list_item,Completetask)
+        // bindInCompleteItems(list_item,Completetask)
 
         new_task.classList.remove('inbutbox_error')
         new_task.classList.add('inbutbox_success')
